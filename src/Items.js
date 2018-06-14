866b9e2c88277ad5b48a3b318f00d3f5654c190c
Shubham Gupta <shubham.n@flipkart.com>
2018-06-14 19:22:07 +0530

    Added general items component

---
+++ src/Items.js
@@ -0,0 +1,100 @@
+import React, { Component } from 'react';
+import ItemsPage from './ItemsPage.js';
+import Onion from './DietImages/onion.jpeg';
+import Potato from './DietImages/potato.jpg';
+import Rice from './DietImages/rice.jpg';
+import Flour from './DietImages/flour.jpg';
+import Ghee from './DietImages/ghee.jpg';
+import Sugar from './DietImages/sugar.jpg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Col } from 'reactstrap';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import AppBar from '@material-ui/core/AppBar';
+import { createMuiTheme } from '@material-ui/core/styles';
+import red from '@material-ui/core/colors/blue';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: red,
+  },
+});
+
+
+
+class Items extends Component {
+	constructor(props){
+		super(props)
+		this.state = ({
+			itemsList : ['onion', 'potato', 'rice', 'flour', 'ghee', 'sugar'],
+		})
+	}
+
+	handleChange = (event, value) => {
+	        if(value === 0) {
+	            this.props.history.push("/items");
+	        }
+	        if(value === 1) {
+	            this.props.history.push("/diets");
+	        }
+	      };
+	    renderDietPlans() {
+	        if(this.props.location.pathname ==="/items") {
+	            return(
+	                <Container>
+	                	<Row>
+	                	 <Col>
+	                	<ItemsPage itemType="Onion" price="20" itemImage={Onion}/>
+	                	</Col>
+	                	<Col>
+	                	<ItemsPage itemType="Potato" price="10" itemImage={Potato}/>
+	                	</Col>
+	                	<Col>
+	                	<ItemsPage itemType="Rice" price="50" itemImage={Rice}/>
+	                	</Col>
+	                	</Row>
+	                	<br />
+	                	<br />
+	                	<Row>
+	                	<Col>
+	                	<ItemsPage itemType="Flour" price="20" itemImage={Flour}/>
+	                	</Col>
+	                	<Col>
+	                	<ItemsPage itemType="Ghee" price="400" itemImage={Ghee}/>
+	                	</Col>
+	                	<Col>
+	                	<ItemsPage itemType="Sugar" price="40" itemImage={Sugar}/>
+	                	</Col>
+	                	</Row>
+	                </Container>
+	            )
+	        }
+	        else {
+	            this.props.history.push("/diets");
+	        }
+	    }
+
+	render(){
+		return (
+			<div>
+			<AppBar position="static" color="default">
+			<Tabs
+			    value={this.state.value}
+			    onChange={this.handleChange}
+			    indicatorColor="primary"
+			    textColor="primary"
+			    centered
+			    >
+		        <Tab label="Items" />
+		        <Tab label="Diet Plans" />
+			</Tabs>
+			</AppBar>
+			<br />
+			<br />
+			{this.renderDietPlans()}
+			</div>
+		);
+	}
+}
+
+export default Items;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import ItemsPage from './ItemsPage.js';
 import { BrowserRouter } from 'react-router-dom';
+import Items from './Items.js';
 
 ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
 registerServiceWorker();
